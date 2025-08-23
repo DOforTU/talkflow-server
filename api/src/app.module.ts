db60827a67710b6a79f295d9620d5e46ed091419
Dohyeon Won <dh1072005@gmail.com>
2025-08-23 20:51:40 +0900

    [UPDATE] App Module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,17 +3,37 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './account/auth/auth.module';
 import { UserModule } from './account/user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SecurityMiddleware } from './common/middleware/security.middleware';
 import { LoggingMiddleware } from './common/middleware/logging.middleware';
 import { ProfileModule } from './account/profile/profile.module';
 import { PrismaService } from './common/prisma/prisma.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PendingUser } from './account/auth/pending-user.entity';
+import { Profile } from './account/profile/profile.entity';
+import { User } from './account/user/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        schema: configService.get('DB_SCHEMA'),
+        entities: [User, Profile, PendingUser],
+        synchronize: configService.get('NODE_ENV') === 'development', // 배포환경에서는 false, 일단 table 자동 생성을 위해 true
+        logging: configService.get('NODE_ENV') === 'development',
+      }),
+      inject: [ConfigService],
     }),
     AuthModule,
     UserModule,
