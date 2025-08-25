b70483c6907d15724416d46c0b96da9a38aa7bad
leejucheol <161808249+leejucheol@users.noreply.github.com>
2025-08-25 18:57:31 +0900

    add: follow

--- api/src/account/follow/follow.controller.ts
+++ api/src/account/follow/follow.controller.ts
@@ -1,4 +1,68 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { FollowService } from './follow.service';
+import { User } from '@prisma/client';
 
-@Controller('follow')
-export class FollowController {}
+@Controller('users')
+export class FollowController {
+  constructor(private readonly followService: FollowService) {}
+
+  // ===== CREATE =====
+
+  @Post(':id/follow')
+  @UseGuards(SmartAuthGuard)
+  async followUser(
+    @Request() req: { user: User },
+    @Param('id') id: number,
+  ): Promise<Follow> {
+    return await this.followService.followUser(req.user.id, id);
+  }
+
+  // ===== READ =====
+
+  /** Get my followers */
+  @Get('me/followers')
+  @UseGuards(SmartAuthGuard)
+  async getMyFollowers(@Request() req: { user: User }): Promise<PublicUser[]> {
+    return toPublicUsers(await this.followService.getFollowers(req.user.id));
+  }
+
+  /** Get my followings */
+  @Get('me/followings')
+  @UseGuards(SmartAuthGuard)
+  async getMyFollowings(@Request() req: { user: User }): Promise<PublicUser[]> {
+    return toPublicUsers(await this.followService.getFollowings(req.user.id));
+  }
+
+  /** Get users who follow me */
+  @Get(':id/followers')
+  async getFollowers(@Param('id') id: number): Promise<PublicUser[] | null> {
+    return toPublicUsers(await this.followService.getFollowers(id));
+  }
+
+  /** Get users I follow */
+  @Get(':id/followings')
+  async getFollowings(@Param('id') id: number): Promise<PublicUser[] | null> {
+    return toPublicUsers(await this.followService.getFollowings(id));
+  }
+
+  // ===== UPDATE =====
+
+  // ===== DELETE =====
+
+  @Delete(':id/unfollow')
+  @UseGuards(SmartAuthGuard)
+  async unfollowUser(
+    @Request() req: { user: User },
+    @Param('id') id: number,
+  ): Promise<boolean> {
+    return await this.followService.unfollowUser(req.user.id, id);
+  }
+}
