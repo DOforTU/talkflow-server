7ac472433f2072efbc38a6fa1529455ac6b385d0
Dohyeon Won <dh1072005@gmail.com>
2025-08-30 18:33:51 +0900

    [RENAME] service

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -8,8 +8,8 @@ import { PrismaModule } from './common/prisma/prisma.module';
 import { UserModule } from './account/user/user.module';
 import { AuthModule } from './account/auth/auth.module';
 import { ProfileModule } from './account/profile/profile.module';
-import { SilhouetteModule } from './silhouette/silhouette/silhouette.module';
-import { SilhouetteLikeModule } from './silhouette/silhouette-like/silhouette-like.module';
+import { SilhouetteModule } from './service/silhouette/silhouette.module';
+import { SilhouetteLikeModule } from './service/silhouette-like/silhouette-like.module';
 import { FileModule } from './common/module/file/file.module';
 
 @Module({
