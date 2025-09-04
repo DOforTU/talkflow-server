8dfc300f915c64c0fb30ce224d514ee5b5cb6f76
leejucheol <161808249+leejucheol@users.noreply.github.com>
2025-09-04 23:55:11 +0900

    update:notice

--- api/src/account/notice/notice.repository.ts
+++ api/src/account/notice/notice.repository.ts
@@ -19,4 +19,17 @@ export class NoticeRepository {
       data: dto,
     });
   }
+
+  // 좋아요와 함께 알림 생성하는 트랜잭션 메서드
+  async createLikeWithNotice(
+    tx: Omit<
+      Prisma.TransactionClient,
+      '$connect' | '$disconnect' | '$on' | '$transaction' | '$use'
+    >,
+    dto: CreateNoticeDto,
+  ) {
+    return await tx.notice.create({
+      data: dto,
+    });
+  }
 }
