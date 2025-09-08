396eb357786b1b0e983860ea8a479499600db7bc
Dohyeon Won <dh1072005@gmail.com>
2025-09-08 11:25:16 +0900

    [ADD] admin 모듈 연결

--- api/src/account/admin/admin.module.ts
+++ api/src/account/admin/admin.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
+import { AdminRepository } from './admin.repository';
 
-@Module({})
+@Module({
+  imports: [],
+  controllers: [AdminController],
+  providers: [AdminService, AdminRepository],
+  exports: [AdminService, AdminRepository],
+})
 export class AdminModule {}
